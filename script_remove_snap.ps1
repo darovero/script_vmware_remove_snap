21bbf785311e7893a717667b0d4ca65f30d56b65
darovero <drvelasquezr@outlook.com>
2021-04-09 12:16:47 -0500

    General adjustments

--- script_remove_snap.ps1
+++ script_remove_snap.ps1
@@ -1,32 +1,54 @@
-Connect-VIserver $viserver -User "administrator@vsphere.local" -Password "Passw0rd."
+################################################################################
+# This script validates all snapshots of VMs in the VMware vSphere environment #
+# and removes VMs that have expired within the defined time for the company    #
+################################################################################
 
+# VARIABLES
 
+#  vCenter Server
+$viserver = "vcsabp.ivti.loc"
+#  User
+$viuser = "administrator@vsphere.local"
+#  Password
+$vipass = "P4ssw0rd."
 # Define the path where the information is stored
 $loc = "C:\VMware_Snap\"
 # Define the file name with extension .txt
 $doc = "VMware_Snap.txt"
-# Define date
-$today = Get-Date -format “yyyy-MM-dd:hh-mm” 
+# Get the date
+$filenameFormat = "Snap_Removed" + " " + (Get-Date -Format "yyyy-MM-dd-hh-mm") + ".txt"
+
+
+# CREATE THE CONNECTION AND ACCESS WITH vCSA
+Connect-VIserver $viserver -User $viuser -Password $vipass
 
 Start-Transcript -Path $loc’remove_snap_log.txt’ -Append
 
-# CREATE FOLDER WHERE THE INFORMATION WILL BE STORED
-if ((Test-Path -Path $loc -PathType Container) -eq $false) {New-Item -Type Directory -Force -Path $loc}
+# VMs validation with Snapshot
+Write-Output "***** The following are the VMs that have Snapshots: ****" "`n" | Out-File $loc$doc -Append
+$snap_validate = Get-VM | Get-Snapshot | Select-Object vm,vmid,name,created,SizeMB
+if ($null -eq $snap_validate) {
+     Write-Output ("No exists VMs with Snapshot")  | Out-File $loc$doc -Append
+}
+else {
+Write-Output $snap_validate | Out-File $loc$doc -Append
+}
 
+# VMs validation with Snapshot for deleted
+Write-Output "`n" "`n"  "**** The following VMs meet the criteria defined by the company to remove the Snapshot ****" "`n" "`n" | Out-File $loc$doc -Append
+$vms_snap = Get-VM  | Get-Snapshot | Where-Object { $_.Created -lt (Get-Date).AddMinutes(-1) } 
+if ($null -eq $vms_snap) {
+     Write-Output ("No exists VMs with Snapshot for delete") | Out-File $loc$doc -Append
+}
+else {
+Write-Output $vms_snap | Select-Object vm,vmid,name,created,SizeMB | Out-File $loc$doc -Append
+}
 
-Write-Output "Starting the script to remove VMware Snapshot" $today "`n" | Out-File $loc$doc -Append
-Write-Output "The following are the VMs that have Snapshots:" $today "`n" | Out-File $loc$doc -Append
-Get-VM | Get-Snapshot | select vm,vmid,name,created,SizeMB | Out-File $loc$doc -Append
-
-
-
-$vms_snap = Get-VM  | Get-Snapshot | Where-Object { $_.Created -lt (Get-Date).AddMinutes(-5) } 
-Write-Output "The following VMs meet the criteria defined by the company to remove the Snapshot" | Out-File $loc$doc -Append
-Write-Output $vms_snap | select vm,vmid,name,created,SizeMB | Out-File $loc$doc -Append
-ForEach ($vm in $vms_snap) {
+ForEach ($vm in $vms_snap)
+{
 	Remove-Snapshot -Snapshot $vm -Confirm:$false
-				}
+}
 
-Write-Output "********************************************************************************" | Out-File $loc$doc -Append
+Rename-Item $loc$doc -NewName $filenameFormat -Force
 
 Stop-Transcript
