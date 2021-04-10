1343c8110cf42d47abf5a7efe4fb28bbb8e0cf32
darovero <drvelasquezr@outlook.com>
2021-04-10 10:07:15 -0500

    snapshot expiration time variable setting

--- script_remove_snap.ps1
+++ script_remove_snap.ps1
@@ -15,9 +15,10 @@ $vipass = "P4ssw0rd."
 $loc = "C:\VMware_Snap\"
 # Define the file name with extension .txt
 $doc = "VMware_Snap.txt"
-# Get the date
+# Rename the file and add the date
 $filenameFormat = "Snap_Removed" + " " + (Get-Date -Format "yyyy-MM-dd-hh-mm") + ".txt"
-
+# Define the expiration date of the Snapshot (for example: AddMinutes for Minutes, AddHours for Hours ando AddDays for day. And change the number value that is inside the parentheses) 
+$validate_time = (Get-Date).AddHours(-24)
 
 # CREATE THE CONNECTION AND ACCESS WITH vCSA
 Connect-VIserver $viserver -User $viuser -Password $vipass
@@ -36,7 +37,7 @@ Write-Output $snap_validate | Out-File $loc$doc -Append
 
 # VMs validation with Snapshot for deleted
 Write-Output "`n" "`n"  "**** The following VMs meet the criteria defined by the company to remove the Snapshot ****" "`n" "`n" | Out-File $loc$doc -Append
-$vms_snap = Get-VM  | Get-Snapshot | Where-Object { $_.Created -lt (Get-Date).AddMinutes(-1) } 
+$vms_snap = Get-VM  | Get-Snapshot | Where-Object { $_.Created -lt $validate_time } 
 if ($null -eq $vms_snap) {
      Write-Output ("No exists VMs with Snapshot for delete") | Out-File $loc$doc -Append
 }
